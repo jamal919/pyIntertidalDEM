c329fddc8903bf53a52ab17544be296fc450411c
silentassasin0111 <silentassasin0111@gmail.com>
2018-07-29 02:18:00 +0600

    Latitude Longitude Data extraction

--- ModuleSentiniel2.py
+++ ModuleSentiniel2.py
@@ -6,7 +6,7 @@ from termcolor import colored          #Visualization ease
 
 import argparse                        #Commandline agrument
 
-from osgeo import gdal                 #GeoTiff file processing
+from osgeo import gdal,osr             #GeoTiff file processing
 
 import matplotlib.pyplot as plt        #Image/Graphical Visualization
 
@@ -230,7 +230,7 @@ def RGB_Construction(unzipped_directory):
     Cloud_mask_data_20m=file_data_validation(mask_files[1])
 
     #EDG
-    Edge_mask_data=file_data_validation(mask_files[2])
+    #Edge_mask_data=file_data_validation(mask_files[2])
     
     #cloud masking correction(bit 1)
    
@@ -315,7 +315,7 @@ def RGB_Construction(unzipped_directory):
     Cloud_mask_data=None
     Cloud_mask_data_20m=None
 
-    Edge_mask_data=None
+    #Edge_mask_data=None
 
     Blue_norm=None
     Red_norm=None
@@ -337,6 +337,76 @@ def RGB_Construction(unzipped_directory):
     return RGB_data    
 
 #-----------------------------------------------------------------------------------------------------------------------------------------------
+def pixel_Coordinate_to_latitude_longitude(data_array):
+    print('')
+    print(colored('*Latitude Longitude Determination ','cyan'))
+    
+    start_time=time.time()
+
+    Total_data_points=np.shape(data_array)[0]
+    #Read Geo Transform data from Geotiff
+
+    directory_strings=str(args.unzipped_directory).split('/')
+    
+    GeoTiff_file=str(args.unzipped_directory)+'/MASKS/'+directory_strings[-1]+'_EDG_R1.tif'
+
+    try:
+        data_set=gdal.Open(GeoTiff_file,gdal.GA_ReadOnly)   #taking readonly data
+    
+    except RuntimeError as e_Read:                       #Error handling
+        print(colored('Error while opening file!','red'))
+        print(colored('Error Details:','blue'))
+        print(e_Read)
+        sys.exit(1)
+
+    [x_offset,pixel_width,rotation_1,y_offset,pixel_height,rotation_2]=data_set.GetGeoTransform()
+
+    #pixel-coordinate to space-coordinate
+
+    pixel_Coordinate_X=data_array[:,0]
+    pixel_Coordinate_y=data_array[:,1]
+
+    Space_coordinate_X= pixel_width * pixel_Coordinate_X +   rotation_1 * pixel_Coordinate_y + x_offset
+    Space_coordinate_Y= rotation_2  * pixel_Coordinate_X + pixel_height * pixel_Coordinate_y + y_offset
+
+
+    #shift to the center of the pixel
+    Space_coordinate_X += pixel_width  / 2.0
+    Space_coordinate_Y += pixel_height / 2.0
+
+    ##get CRS from dataset
+    Coordinate_Reference_System=osr.SpatialReference()                     #Get Co-ordinate reference
+    Coordinate_Reference_System.ImportFromWkt(data_set.GetProjectionRef()) #projection reference
+
+    ## create lat/long CRS with WGS84 datum<GDALINFO for details>
+    Coordinate_Reference_System_GEO=osr.SpatialReference()
+    Coordinate_Reference_System_GEO.ImportFromEPSG(4326)                   # 4326 is the EPSG id of lat/long CRS
+
+    Transform_term = osr.CoordinateTransformation(Coordinate_Reference_System, Coordinate_Reference_System_GEO)
+
+    
+    
+    latitude_data=np.zeros(Total_data_points)
+    longitude_data=np.zeros(Total_data_points)
+    
+    
+
+    for indice in range(0,Total_data_points):
+        (latitude_point, longitude_point, _ ) = Transform_term.TransformPoint(Space_coordinate_X[indice], Space_coordinate_Y[indice])
+        
+        latitude_data[indice]=latitude_point
+        longitude_data[indice]=longitude_point
+        
+
+
+    print('')
+    print(colored("Total Elapsed Time: %s seconds " % (time.time() - start_time),'green'))
+
+    return np.column_stack((latitude_data,longitude_data))
+
+    
+
+#-----------------------------------------------------------------------------------------------------------------------------------------------
 
 def module_run():
     start_time = time.time()
@@ -348,7 +418,7 @@ def module_run():
     print('')
     print(colored('*Reading RGB data ','cyan'))
     
-    [row,col,dim]=np.shape(rgb_data)
+    [row,col,_]=np.shape(rgb_data)
 
     #hsv conversion
     print('')
@@ -423,7 +493,7 @@ def module_run():
     hue_data=None
     val_data=None
 
-    Plot_with_Geo_ref(Map_Water,'Water val Channel')
+    Plot_with_Geo_ref(Map_Water,'Water Map')
 
     print('')
     print(colored('*Calculating Convoluted Data ','cyan'))
@@ -441,9 +511,11 @@ def module_run():
     convoluted_data=None
     Map_Water=None
 
+    latitude_longitude=pixel_Coordinate_to_latitude_longitude(Map_Shore)
+    
+    debug_print_value(latitude_longitude,'latitude_longitude')
 
-
-    #plt.show()
+    plt.show()
 
 
 #Main 
