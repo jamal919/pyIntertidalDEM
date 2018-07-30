21a4794c003ab01f45402d78390e5ba660f16cd9
silentassasin0111 <silentassasin0111@gmail.com>
2018-07-30 14:32:49 +0600

    Clean Commit

--- Sentiniel2Logger.py
+++ Sentiniel2Logger.py
@@ -1,4 +1,4 @@
-import time,os,simplekml,shapefile,matplotlib.pyplot as plt,numpy as np,sys,gc 
+import time,os,simplekml,shapefile,matplotlib.pyplot as plt,numpy as np,sys,gc,csv 
 from termcolor import colored
 from osgeo import gdal
     
@@ -75,6 +75,50 @@ class Log(object):
         print('')
         print(colored("Elapsed Time(GeoTiff Saving): %s seconds " % (time.time() - start_time),'green'))
 
+    def SaveDataAsCSV(self,Identifier,Data):
+        start_time=time.time()  
+        csvfile=self.OutputDir+str(Identifier)+'.csv'
+        with open(csvfile,"w") as output:
+            writer=csv.writer(output,lineterminator='\n')
+            for index in range(0,np.shape(Data)[0]):
+                writer.writerow(Data[index].tolist())
+        self.PrintLogStatus('Saving '+str(Identifier)+'.csv')
+
+        print('')
+        print(colored("Elapsed Time(CSV Saving): %s seconds " % (time.time() - start_time),'green'))
+    
+    def SaveDataAsKML(self,Identifier,Data):
+        start_time = time.time()
+        outputfile=self.OutputDir+str(Identifier)+'.kml'
+        kml=simplekml.Kml()
+        for index in range(0,np.shape(Data)[0]):
+            __lat=Data[index,0]
+            __lon=Data[index,1]
+            kml.newpoint(coords=[(__lat,__lon)])
+        kml.save(outputfile)
+        self.PrintLogStatus('Saving '+str(Identifier)+'.kml')
+
+        print('')
+        print(colored("Elapsed Time(kml Saving): %s seconds " % (time.time() - start_time),'green'))
+   
+    def SaveDataAsSHPPoint(self,Identifier,Data):
+        start_time=time.time()
+        outputfile=self.OutputDir+str(Identifier)+'__point.shp'
+
+        __lat=Data[:,0]
+        __lon=Data[:,1]
+
+        Writer_SHP=shapefile.Writer(shapefile.POINT)
+        Writer_SHP.field('Latitude')
+        Writer_SHP.field('Longitude')
+        Writer_SHP.record(__lat,__lon)
+        Writer_SHP.save(outputfile)
+
+        self.PrintLogStatus('Saving '+str(Identifier)+'_point.shp')
+
+        print('')
+        print(colored("Elapsed Time(shp Saving): %s seconds " % (time.time() - start_time),'green'))
+   
 class DebugLog(object):
     def __init__(self,Directory):
         self.Directory=Directory
@@ -123,66 +167,3 @@ class DebugLog(object):
         self.__ReturnData()
 
         return self.__data
-
-'''
-##output formats---later process    
-
-    csvfile=str(os.getcwd())+'/Output_log/'+str(directory_strings[-1])+'.csv'
-
-    with open(csvfile,"w") as output:
-        writer=csv.writer(output,lineterminator='\n')
-        for index in range(0,np.shape(latitude_longitude)[0]):
-            writer.writerow(latitude_longitude[index].tolist())
-    
-
-def kml_output(latitude_longitude,unzipped_directory):
-    
-    start_time = time.time()
-
-    directory_strings=str(unzipped_directory).split('/')
-
-    outputfile=str(os.getcwd())+'/Output_log/'+str(directory_strings[-1])+'.kml'
-
-    kml=simplekml.Kml()
-
-    for index in range(0,np.shape(latitude_longitude)[0]):
-        lat=latitude_longitude[index,0]
-        lon=latitude_longitude[index,1]
-        kml.newpoint(coords=[(lat,lon)])
-    
-    kml.save(outputfile)
-
-    print('')
-    print(colored('*Saved to kml file ','cyan'))
-    
-    print('')
-    print(colored("Elapsed Time: %s seconds " % (time.time() - start_time),'green'))
-
-
-
-
-def shape_point_output(latitude_longitude,unzipped_directory):
-    
-    start_time=time.time()
-
-    directory_strings=str(unzipped_directory).split('/')
-
-    outputfile=str(os.getcwd())+'/Output_log/'+str(directory_strings[-1])+'__point.shp'
-
-    lat=latitude_longitude[:,0]
-    lon=latitude_longitude[:,1]
-
-    Writer_SHP=shapefile.Writer(shapefile.POINT)
-    
-    Writer_SHP.field('Latitude')
-    Writer_SHP.field('Longitude')
-
-    Writer_SHP.record(lat,lon)
-    Writer_SHP.save(outputfile)
-
-    print('')
-    print(colored('*Saved to .shp file ','cyan'))
-    
-    print('')
-    print(colored("Elapsed Time: %s seconds " % (time.time() - start_time),'green'))
-'''
