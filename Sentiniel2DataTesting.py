b91f8ec2b9f773a2f08e00c5963c95f1eea4517d
silentassasin0111 <silentassasin0111@gmail.com>
2018-08-04 08:19:16 +0600

    Clean Run

--- Sentiniel2DataTesting.py
+++ Sentiniel2DataTesting.py
@@ -6,17 +6,28 @@ class DataTester(object):
         self.Directory=Directory
         self.Data=Data
         self.__Logger=Log(self.Directory)
-        self.__kernel=np.ones((3,3),np.uint8)
-
+        
+    def __PlotUpToSegments(self,num):
+        __BackGround=np.zeros(np.shape(self.Data))
+        for i in range(2,num):
+            PointValueToConsider=self.__SortedByFeature[-i] #based on highest number of occuerence
+            if i%5==0:
+                __val=1
+            else :
+                __val=i%5
+            __BackGround[self.__Labeled_array==PointValueToConsider]=__val
+        self.__Logger.DebugPlot(__BackGround,'Segment:1-'+str(num)+' top data points')
+        
+    def __FilterUpToPixelCount(self,ThreshHold):
+        __FeatureListCount=np.shape(np.where(self.__CountsOfFeature<=ThreshHold))[1]
+        __FeatureValues=np.column_stack(np.where(self.__CountsOfFeature<=ThreshHold))
+        __SignificantFeatureCount=self.__TotalFeatures - __FeatureListCount
+        self.__PlotUpToSegments(__SignificantFeatureCount)
     def SegmentationWaterMap(self):
         start_time=time.time()
-        __Labeled_array, _ =scipy.ndimage.measurements.label(self.Data)
-        _, __CountsOfFeature = np.unique(__Labeled_array, return_counts=True)
-        __SortedByFeature=np.argsort(__CountsOfFeature)
-        __BackGround=np.zeros(np.shape(self.Data))
-        for i in range(2,7):
-            
-            PointValueToConsider=__SortedByFeature[-i] #based on highest number of occuerence
-            __BackGround[__Labeled_array==PointValueToConsider]=i-1+5
-        self.__Logger.DebugPlot(__BackGround,'Segment:1-5 top data points')
+        self.__Labeled_array, _ =scipy.ndimage.measurements.label(self.Data)
+        __FeatureNumber, self.__CountsOfFeature = np.unique(self.__Labeled_array, return_counts=True)
+        self.__SortedByFeature=np.argsort(self.__CountsOfFeature)
+        self.__TotalFeatures=np.shape(self.__CountsOfFeature)[0]
+        self.__Logger.SaveDataAsCSV('FeatureData',np.column_stack([__FeatureNumber,self.__CountsOfFeature]))
         print(colored("Total Elapsed Time(Segmentation): %s seconds " % (time.time() - start_time),'green'))
