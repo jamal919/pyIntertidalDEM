1304dc3b2de3f2e74415fbfe374093a157cdfb79
silentassasin0111 <silentassasin0111@gmail.com>
2018-08-13 20:22:33 +0600

    Clean Commit

--- main.py
+++ main.py
@@ -5,13 +5,14 @@ from Sentiniel2Logger import Log,DebugLog
 from Sentiniel2Preprocessor import Preprocessor
 from Sentiniel2RGBProcessor import RGBProcessor
 from Sentiniel2DataFilter import DataFilter
+from Sentiniel2GeoData import GeoData
 import matplotlib.pyplot as plt,numpy as np,argparse,time
 from termcolor import colored
 import os
 
 testCase1="/home/ansary/Sentiniel2/Data/20171130/SENTINEL2B_20171130-042157-149_L2A_T46QCK_D_V1-4"
 testCase2="/home/ansary/Sentiniel2/Data/20180224/SENTINEL2B_20180224-045147-074_L2A_T45QYE_D/SENTINEL2B_20180224-045147-074_L2A_T45QYE_D_V1-5"  
-directory=testCase2
+directory=testCase1
 
 
 def ModuleRun(directory):
@@ -37,15 +38,15 @@ def ModuleRun(directory):
 
     IsWater[NoData==1]=0
     
-    Logger.SaveArrayToGeotiff(IsWater,'IsWater')
-    
-    #Filter=DataFilter(directory,IsWater)           #Data tester object
+    Filter=DataFilter(directory,IsWater)
 
-    #WaterMap=Filter.FilterWaterMap(5000)
+    WaterMap=Filter.FilterWaterMap()
 
-    #GeoObj=GeoData(directory,MapWater)
+    Logger.DebugPlot(WaterMap,'WaterMap')
+
+    Geo=GeoData(directory,WaterMap)
     
-    #LatLon=GeoObj.GetShoreLineGeoData()
+    LatLon=Geo.GetShoreLineGeoData()
     
     #Logger.DebugPrint(LatLon,'Lat Lon')
 
@@ -57,23 +58,5 @@ def ModuleRun(directory):
 
     plt.show()
     
-def DebugRun(directory):
-    Logger=Log(directory)
-    
-    DebugLogger=DebugLog(directory) 
-    
-    DataFile=Logger.OutputDir+'IsWater.tiff'
-    
-    IsWater=DebugLogger.GetFileData(DataFile)
-
-    Filter=DataFilter(directory,IsWater)
-
-    WaterMap=Filter.FilterWaterMap()
-
-    Logger.DebugPlot(WaterMap,'WaterMap')
-
-    plt.show()
-    
-    
 if __name__=='__main__':
-    DebugRun(directory)
+    ModuleRun(directory)
