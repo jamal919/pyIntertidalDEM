e5948fb58cb55047ac672d862c7cff782d7fb95d
silentassasin0111 <silentassasin0111@gmail.com>
2018-09-03 09:24:18 +0600

    Phase

--- DataAnalysis/DataTrain.py
+++ DataAnalysis/DataTrain.py
@@ -1,18 +1,30 @@
 #!/usr/bin/env python3
-from DataList import ListTrainingData
+#from DataList import ListTrainingData
+from Sentiniel2Logger import ViewData,TiffReader,SaveData
 import numpy as np
-import cv2
 import matplotlib.pyplot as plt
-
     
 def test(TDF):
-    JPGPath='/home/ansary/Sentiniel2/Data/'+str(TDF)+'/'+str(TDF)+'_QKL_ALL.jpg'
-    img = cv2.imread(JPGPath,cv2.IMREAD_GRAYSCALE) # reads image as grayscale
-    plt.imshow(img, cmap='gray')
+    #Directory='/home/ansary/Sentiniel2/Data/'+TDF+'/'
+    #TR=TiffReader(Directory)
+    #DataFile='/home/ansary/Sentiniel2/RGBUNQ/'+TDF+'/Data.tiff'
+    #Data=TR.GetTiffData(DataFile)
+    #VD=ViewData(Directory)
+    #VD.PlotWithGeoRef(Data,'Data')    
+    #SD=SaveData(Directory)
+    #Feature,CountsOfFeature = np.unique(Data, return_counts=True)
+    #STORE=np.column_stack((Feature,CountsOfFeature))
+    #SD.SaveDataAsCSV('Feature',STORE)
+    ImagePath='/home/ansary/Sentiniel2/RGBData/'+TDF+'/'+'RGBData.jpg'
+    img=plt.imread(ImagePath)
+    plt.imshow(img)
     plt.show()
+
 def main():
-    TrainData=ListTrainingData()
-    test(TrainData[0])
+    #TrainData=ListTrainingData()
+    test('SENTINEL2B_20171130-042157-149_L2A_T46QCK_D_V1-4')
 
 if __name__=='__main__':
     main()
+    plt.show()
+    
