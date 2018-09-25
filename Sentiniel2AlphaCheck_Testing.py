f87c50afde16f62ff626c72e1519e9b9bf123eb4
silentassasin0111 <silentassasin0111@gmail.com>
2018-09-25 15:14:24 +0600

    Alpha Test

--- Sentiniel2AlphaCheck_Testing.py
+++ Sentiniel2AlphaCheck_Testing.py
@@ -1,7 +1,7 @@
-import time,matplotlib,numpy as np,gc,sys
+import time,matplotlib,numpy as np,gc,sys,matplotlib.pyplot as plt,os
 from osgeo import gdal
 from Sentiniel2Logger import TiffReader,TiffWritter,ViewData,Info
-class Processor(object):
+class Test_Processor(object):
     '''
         Process the Hue and Value channel to construct a binary waterMap
     '''
@@ -9,9 +9,7 @@ class Processor(object):
         __InfoObj=Info(Directory)
         self.__InputFolder=__InfoObj.OutputDir('TIFF')
         
-        self.IdentifierS=str(self.__InputFolder).split('/')
-        self.IdentifierS=list(filter(bool,self.IdentifierS))
-        
+        self.OUTdir=__InfoObj.OutputDir('PNG')
 
         self.DataViewer=ViewData(Directory)
         self.Directory=Directory
@@ -20,15 +18,102 @@ class Processor(object):
        
      ##Saving Necessary Results
 
-    def __SaveChannelData(self,Data,Identifier,SaveGeoTiff=False):
+    def __SaveChannelData(self,Data,Identifier,SaveGeoTiff=False,SaveDIR=None):
         '''
             Save's the Channel data as TIFF and PNG
         '''
-        self.DataViewer.PlotWithGeoRef(Data,str(Identifier))
+        if SaveDIR is None:
+            self.DataViewer.PlotWithGeoRef(Data,str(Identifier))
+        else:
+            self.DataViewer.PlotWithGeoRef(Data,str(Identifier),TestSaveDir=str(SaveDIR))
         
         if(SaveGeoTiff==True):
             self.TiffWritter.SaveArrayToGeotiff(Data,str(Identifier))
 
+    def __PlotHistogramOfAlpha(self):
+        print('Getting Alpha Channel')
+        __File=self.__InputFolder+"1.1.3 Alpha Band N.tiff"
+        
+        Alpha=self.TiffReader.GetTiffData(__File)
+        
+        
+        
+        Value,Count= np.unique(Alpha, return_counts=True)
+        
+        Vl=np.nanmin(Value)
+        
+        Vh=np.nanmax(Value)
+        
+        V=np.linspace(Vl,Vh,10,endpoint=True)
+
+        Cl=np.nanmin(Count)
+
+        Ch=np.nanmax(Count)
+        
+        C=np.linspace(Cl,Ch,10,endpoint=True)
+
+        plt.figure('Alpha Histogram of values')
+            
+        plt.title('Alpha Histogram of values')
+        
+        plt.grid(True)
+
+        plt.xticks(V)
+
+        plt.yticks(C)
+        
+        plt.xlabel('Normalized Values')
+        
+        plt.ylabel('Value Count')
+        
+        plt.plot(Value,Count)
+
+        plt.savefig(self.OUTdir+'Test_0_CompleteAlphaHistogram.png')
+
+        #clear memory
+        plt.clf()
+        
+        plt.close()
+
+        STD=np.nanstd(Alpha)
+        iSTD=(Value<=1.1*STD) & ~np.isnan(Value)
+        Count=Count[iSTD]
+        Value=Value[iSTD]
+
+        V=np.linspace(0,1.1*STD,11,endpoint=True)
+        Cl=np.nanmin(Count)
+        Ch=np.nanmax(Count)
+        C=np.linspace(Cl,Ch,10,endpoint=True)
+
+        Vlbl=[]
+        Vlbl.append('0')
+        for i in range(1,10):
+            vstr='0.'+str(i)+' S'
+            Vlbl.append(str(vstr))
+        Vlbl.append('S')
+
+        plt.figure('Alpha Histogram of STD values')
+            
+        plt.title('Alpha Histogram of STD values')
+        
+        plt.grid(True)
+
+        plt.xticks(V,Vlbl)
+
+        plt.yticks(C)
+        
+        plt.xlabel('Normalized STD Values')
+        
+        plt.ylabel('Value Count (STD)')
+        
+        plt.plot(Value,Count)
+
+        plt.savefig(self.OUTdir+'Test_0_STDAlphaHistogram.png')
+
+        #clear memory
+        plt.clf()
+        
+        plt.close()
 
     def __LoadHueValue(self):
         '''
@@ -42,8 +127,8 @@ class Processor(object):
         print('Getting Hue Data')
         __File=self.__InputFolder+"2.2.1_HUE_Normalized_Pekel.tiff"
         self.Hue=self.TiffReader.GetTiffData(__File)
-    
-    def __CreateWaterMask(self): 
+        
+    def __CreateWaterMask(self,Div): 
         '''
             A thresh hold is selected for which Alpha is clipped to 0 to form a water mask
         '''   
@@ -56,24 +141,35 @@ class Processor(object):
         self.iNan=np.isnan(Alpha)
 
         self.WaterMask=np.zeros(Alpha.shape)
-        ##
-        #Alpha=np.round(Alpha,decimals=1)
-        self.WaterMask[Alpha<0.5*np.nanstd(Alpha)]=1  ##Changeable
-        #self.WaterMask[Alpha<0.1]=1
-
+        
+        self.WaterMask[Alpha<(np.nanstd(Alpha)*Div)]=1  ##Changeable
+        
         self.WaterMask[self.iNan]=np.nan
         
-        self.__SaveChannelData(self.WaterMask,str(self.IdentifierS[-1])+'__WaterMaskFixed')
-    
+        self.DIVStd=Div
+
+        self.TestSavePNGDir=self.OUTdir+'STD_Scale_'+str(self.DIVStd)+'/'
+        
+        if not os.path.exists(self.TestSavePNGDir):
+            os.mkdir(self.TestSavePNGDir)
+
+        self.__SaveChannelData(self.WaterMask,'Test__STD_'+str(self.DIVStd)+'_3.1.0 Water Mask__ThreshValue='+str((np.nanstd(Alpha)*Div)),SaveDIR=self.TestSavePNGDir)
+
+       
+
+
+
     def __MaskHueValue(self):
         print('Masking Value Channel with water mask')
         self.Value[self.WaterMask==0]=np.nan
-        self.__SaveChannelData(self.Value,'3.1.1 Masked Value Channel')
+        self.__SaveChannelData(self.Value,'Test__STD_'+str(self.DIVStd)+'_3.1.1 Masked Value Channel',SaveDIR=self.TestSavePNGDir)
 
         print('Inverse Masking Value Channel with water mask')
         self.Hue[self.WaterMask==1]=np.nan
-        self.__SaveChannelData(self.Hue,'3.1.3 Inversed Masked Hue Channel')
+        self.__SaveChannelData(self.Hue,'Test__STD_'+str(self.DIVStd)+'_3.1.3 Inversed Masked Hue Channel',SaveDIR=self.TestSavePNGDir)
 
+    
+    
     def __FormBinaryWaterValueChannel(self):
         '''
             BW_value is formed as:
@@ -102,9 +198,11 @@ class Processor(object):
         self.BW_Value=self.BW_Value.astype(np.float)
         self.BW_Value[self.iNan]=np.nan
         
-        self.__SaveChannelData(self.BW_Value,'3.1.2 Binary Water Value Channel:SF='+str(n))
+        self.__SaveChannelData(self.BW_Value,'Test__STD_'+str(self.DIVStd)+'_3.1.2 Binary Water Value Channel_SF='+str(n),SaveDIR=self.TestSavePNGDir)
     
-    def __FormBinaryWaterHueChannel(self):
+    
+    
+    def __FormBinaryWaterHueChannel(self,SF):
         
         '''
             BW_hue is formed as:
@@ -119,7 +217,8 @@ class Processor(object):
         T=np.nanmedian(self.Hue)       #Median 
         S=np.nanstd(self.Hue)          #standard deviation
         
-        n=0.4                            #Scaling Factor
+        n=SF 
+        self.SFn=SF                           #Scaling Factor
 
         #Value channel conditional constant 
         c1=T+n*S
@@ -133,7 +232,7 @@ class Processor(object):
         self.BW_Hue=self.BW_Hue.astype(np.float)
         self.BW_Hue[self.iNan]=np.nan
         
-        self.__SaveChannelData(self.BW_Hue,'3.1.4 Binary Water Hue Channel:SF='+str(n))
+        self.__SaveChannelData(self.BW_Hue,'Test__STD_'+str(self.DIVStd)+'__SF='+str(n)+'_3.1.4 Binary Water Hue Channel',SaveDIR=self.TestSavePNGDir)
         
     def __AndOperationWaterMap(self):
         IsWater=np.zeros(self.BW_Hue.shape)
@@ -142,7 +241,7 @@ class Processor(object):
         IsWater=IsWater.astype(np.float)
         IsWater[self.iNan]=np.nan
         
-        self.__SaveChannelData(IsWater,'3.1.5 Binary Water Map')        
+        self.__SaveChannelData(IsWater,'Test__STD_'+str(self.DIVStd)+'__SF='+str(self.SFn)+'_3.1.5 Binary Water Map',SaveDIR=self.TestSavePNGDir)        
         
         
     def GetBinaryWaterMap(self):
@@ -155,10 +254,16 @@ class Processor(object):
             > Birany Hue Water
             > And operation
         '''
+        #self.__PlotHistogramOfAlpha()
+        
         self.__LoadHueValue()
-        self.__CreateWaterMask()
-        self.__MaskHueValue()
-        self.__FormBinaryWaterValueChannel()
-        self.__FormBinaryWaterHueChannel()
-        self.__AndOperationWaterMap()
+        WSF=[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1]
+        for sc in WSF:
+            self.__CreateWaterMask(sc)
+            self.__MaskHueValue()
+            self.__FormBinaryWaterValueChannel()
+            SF=[0.3,0.4,0.5,0.6,0.7,0.8,0.9,1,1.1,1.2,1.3,1.4,1.5]
+            for sf in SF: 
+                self.__FormBinaryWaterHueChannel(sf)
+                self.__AndOperationWaterMap()
         
