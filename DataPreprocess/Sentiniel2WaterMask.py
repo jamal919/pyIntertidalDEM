e9e678e2c8208e53b8e18a5485306766fe2f6ba5
silentassasin0111 <silentassasin0111@gmail.com>
2018-10-01 03:42:19 +0600

    FINALIZING-1.0.0

---
+++ DataPreprocess/Sentiniel2WaterMask.py
@@ -0,0 +1,194 @@
+#!/usr/bin/env python3
+import time
+import matplotlib
+import numpy as np
+import sys
+import matplotlib.pyplot as plt
+import os
+import scipy.ndimage
+from osgeo import gdal,osr
+
+
+
+
+##Read DataSet
+def ReadTiffData(File):
+    '''
+        Reads the Dataset
+    '''
+    gdal.UseExceptions()
+    try:
+        __DataSet=gdal.Open(File,gdal.GA_ReadOnly)        #taking readonly data
+    
+    except RuntimeError as e_Read:                             #Error handling
+        print('Error while opening file!')
+        print('Error Details:')
+        print(e_Read)
+        sys.exit(1)
+    return __DataSet
+##Read Raster Data
+def GetTiffData(File):
+    '''
+        Returns single Raster data as array
+    '''
+    __DataSet=ReadTiffData(File)
+
+    if(__DataSet.RasterCount==1):                          
+        try:
+            __RasterBandData=__DataSet.GetRasterBand(1)
+            
+            __data=__RasterBandData.ReadAsArray()
+            
+        except RuntimeError as e_arr:                                   #Error handling
+            print('Error while data extraction file!')
+            print('Error Details:')
+            print(e_arr)
+            sys.exit(1)
+        return __data
+    else:
+        print('The file contains Multiple bands')
+        sys.exit(1)
+
+##Save Geotiff
+def SaveArrayToGeotiff(Array,DIRGTIFF,Identifier):
+    OutDir=str(os.getcwd())+'/'
+    if str(Identifier).find('Zone')!=-1:
+        OutDir=OutDir+'Zone/'
+        if not os.path.exists(OutDir):
+            os.mkdir(OutDir)
+    if str(Identifier).find('WaterMask')!=-1:
+        OutDir=OutDir+'WaterMask/'
+        if not os.path.exists(OutDir):
+            os.mkdir(OutDir)
+    if str(Identifier).find('Filtered')!=-1:
+        OutDir=OutDir+'Filtered/'
+        if not os.path.exists(OutDir):
+            os.mkdir(OutDir)
+    
+
+    print('Saving:'+str(Identifier))
+    DataSet=ReadTiffData(DIRGTIFF)
+    Projection=DataSet.GetProjection()
+    GeoTransform=DataSet.GetGeoTransform()    
+    
+    GeoTiffFileName =OutDir+str(Identifier)+'.tiff'   # Output geotiff file name according to identifier
+    
+    Driver = gdal.GetDriverByName('GTiff')
+    OutputDataset = Driver.Create(GeoTiffFileName,np.shape(Array)[0],np.shape(Array)[1], 1,gdal.GDT_Float32)
+    OutputDataset.GetRasterBand(1).WriteArray(Array)
+    OutputDataset.SetGeoTransform(GeoTransform)
+    OutputDataset.SetProjection(Projection)
+    OutputDataset.FlushCache()
+    print('Saved:'+str(Identifier))
+
+##CloudMask
+def CloudMaskCorrection(BandData,MaskData):
+    Decimals=GetDecimalsWithEndBit(np.amax(MaskData))
+    for v in range(0,len(Decimals)):
+        BandData[MaskData==Decimals[v]]=-10000                #Exclude data point Identifier= - Reflectance value
+    return BandData 
+
+def GetDecimalsWithEndBit(MaxValue):
+    results=[]
+    for i in range(0,MaxValue+1):
+        BinaryString=format(i,'08b')
+        if(BinaryString[-1]=='1'):
+            results.append(i)
+    return results
+
+
+
+def ProcessAlpha(Directory,AllData):
+    DirectoryStrings=str(Directory).split('/')             #split the directory to extract specific folder
+        
+    DirectoryStrings=list(filter(bool,DirectoryStrings))
+
+    SWIRB11File=str(Directory)+str(DirectoryStrings[-1])+'_FRE_B11.tif'
+    SWIRB12File=str(Directory)+str(DirectoryStrings[-1])+'_FRE_B12.tif'
+    CloudMask20m=str(Directory)+'/MASKS/'+str(DirectoryStrings[-1])+'_CLM_R2.tif'
+    print('Processing Alpha Data:'+str(DirectoryStrings[-1]))
+    B11=GetTiffData(SWIRB11File)
+    B12=GetTiffData(SWIRB12File)
+    CLM=GetTiffData(CloudMask20m)
+
+    B11=CloudMaskCorrection(B11,CLM)
+    B12=CloudMaskCorrection(B12,CLM)
+
+    B11=np.array(B11.repeat(2,axis=0).repeat(2,axis=1))
+    B12=np.array(B12.repeat(2,axis=0).repeat(2,axis=1))
+    
+    B11=B11.astype(np.float)
+    B12=B12.astype(np.float)
+    
+    
+    iPosB11=(B11==-10000)
+    iPosB12=(B12==-10000)
+
+    B11[iPosB11]=AllData[iPosB11]
+    B12[iPosB11]=AllData[iPosB12]
+    
+
+    B11=(B11-np.nanmin(B11))/(np.nanmax(B11)-np.nanmin(B11))
+    B12=(B12-np.nanmin(B12))/(np.nanmax(B12)-np.nanmin(B12))
+
+    Alpha=B11+B12
+
+    Alpha=(Alpha-np.nanmin(Alpha))/(np.nanmax(Alpha)-np.nanmin(Alpha))
+    
+    return Alpha
+    
+    
+def main(directory,Zones):    
+
+    DataPath=directory
+    
+    for zone in Zones:
+        DataPath=DataPath+str(zone)+'/'
+        print('Executing Module for zone:'+str(zone))
+        DataFolders=os.listdir(path=DataPath)
+        DirGtiff=DataPath+str(DataFolders[0])+'/'+str(DataFolders[0])+'_FRE_B8.tif'#10m resolution Size (10980,10980)
+        Gtiff=GetTiffData(DirGtiff)
+       
+        All=np.zeros(Gtiff.shape)
+
+        for df in DataFolders:
+            dirc=DataPath+df+'/'
+            Alpha=ProcessAlpha(dirc,All)
+            All=All+Alpha
+            All=(All-np.nanmin(All))/(np.nanmax(All)-np.nanmin(All))
+        
+        DataPath=directory
+        
+        SaveArrayToGeotiff(All,DirGtiff,str(zone)+'__Zone')
+        
+        
+def FilterWaterMask(DataFile,zone):
+    print('Filtering:'+str(DataFile))
+    Data=GetTiffData(DataFile)
+    LabeledData,_=scipy.ndimage.measurements.label(Data)
+    Value,PixelCount=np.unique(LabeledData,return_counts=True)
+    MaxPixel=np.amax(PixelCount[Value>0])
+    MaxVal=Value[PixelCount==MaxPixel]
+    WaterMask_Filtered_File=str(zone)+'__Filtered'
+    WF=np.zeros(Data.shape)
+    WF[LabeledData==MaxVal[0]]=1
+    SaveArrayToGeotiff(WF,DataFile,WaterMask_Filtered_File)
+
+if __name__=='__main__':
+    directory=input('The Directory of the Data:')
+    Zones=os.listdir(directory)
+    main(directory,Zones)
+    
+    
+    for zone in Zones:
+        
+        ZFile=str(os.getcwd())+'/Zone/'+str(zone)+'__Zone.tiff'
+        Data=GetTiffData(ZFile)
+        Data=Data/np.nanstd(Data)
+        WF=np.ones(Data.shape)
+        WF[Data>0.5]=0
+        print('Saving WaterMask For:'+str(zone))
+        SaveArrayToGeotiff(WF,ZFile,str(zone)+'__WaterMask')
+        
+        Dfile=str(os.getcwd())+'/WaterMask/'+str(zone)+'__WaterMask.tiff'
+        FilterWaterMask(Dfile,zone)
