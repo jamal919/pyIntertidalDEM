f05902ebda480aaf486e73d5e40338264ddf81c6
silentassasin0111 <silentassasin0111@gmail.com>
2018-10-24 00:13:14 +0600

    HSV

--- improc/__init__.py
+++ improc/__init__.py
@@ -1,6 +1,19 @@
 # -*- coding: utf-8 -*-
 from .band_processor import BandData
- 
-def construct_channels(directory,improcdir, tiff=True, png=True):
+from .HSV_processor import HSVData
+
+def construct_channels(directory,OUTDIR,png=False):
+    SavePNG=False
+    if png:
+        SavePNG=True
+
+    BandDataObj=BandData(directory,OUTDIR,PNGFLAG=SavePNG)
+    BandDataObj.Data()
     
-    BandDataObj=BandData(directory,improcdir)
+    HSVIN=BandDataObj.TIFFSAVEDIR
+    if png:
+        PNGDIRHSV=BandDataObj.PNGDIR_FINAL
+        HSVDataObj=HSVData(HSVIN,PNGdir=PNGDIRHSV)
+    else:
+        HSVDataObj=HSVData(HSVIN)
+    HSVDataObj.HueValueRGB()
