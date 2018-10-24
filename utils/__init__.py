4c4e1ac0f0abc45dbcef46f20bfaec7859cf290a
silentassasin0111 <silentassasin0111@gmail.com>
2018-10-24 23:47:15 +0600

    PLOT CHECK

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,12 @@
 # -*- coding: utf-8 -*-
+from utils.dataplotter import DataPlotter
+from utils.tiffreader import TiffReader
+def mapPlotTest():
+    Reader=TiffReader()
+    
+    RefTiff='/media/ansary/My Passport/OUTPUT/PreProcessed/WaterMask/T45QWE.tiff'
+    OutDir='/media/ansary/My Passport/'
+    data=Reader.GetTiffData(RefTiff)
+
+    DataViewer=DataPlotter(RefTiff,OutDir)
+    DataViewer.plotInMap(data)
