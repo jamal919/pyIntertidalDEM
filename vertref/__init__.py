f634d59e315490033afbe0c1833749ff1f3f7105
silentassasin0111 <silentassasin0111@gmail.com>
2018-10-27 18:50:34 +0600

    Utide issue solved

--- vertref/__init__.py
+++ vertref/__init__.py
@@ -2,7 +2,7 @@
 from .tide import Analyzer
 import os
 
-def save_constituents(dem_data_dir,dem_output_dir):
+def save_constituents(dem_data_dir,vertrefdir):
     dem_data_dir=os.path.join(dem_data_dir,'Chandpur')
     print('Saving constituents:',str(dem_data_dir))
     AnalyzerObj=Analyzer(dem_data_dir)
