657e51f8e676c2ca51d8959a473aed705bf155bc
silentassasin0111 <silentassasin0111@gmail.com>
2018-10-28 22:00:54 +0600

    TTST

--- vertref/dem.py
+++ vertref/dem.py
@@ -4,7 +4,22 @@ import numpy as np
 
 
 
+class Predictor(object):
+    def __init__(self,dem_input_dir):
+        self.__indir=dem_input_dir
 
+    def listStation(self):
+        pass
+
+    def findClosest(self):
+        # Find closest station correspods to a given lat long
+        pass
+
+    def calcWL(self):
+        # Load closest station constituent values
+        # calculate WL for a given time
+        # return WL
+        pass
 class Dem(object):
     def __init__(self):
         pass
