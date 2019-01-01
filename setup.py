e2074d384540e6131c307cf06e2968004b41ecae
Jamal Khan <jamal919@gmail.com>
2019-01-01 18:58:50 +0100

    distribution setting added

---
+++ setup.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Setup for pyIntertidalDEM
+
+@author: khan
+"""
+
+import setuptools
+
+setuptools.setup(
+    name='pyIntertidalDEM',
+    version='0.1',
+    author='MJUK and NA',
+    author_email='jamal.khan@legos.obs-mip.fr',
+    description='Intertidal DEM generation Package',
+    packages=setuptools.find_packages(),
+    license='MIT',
+    install_requires=[
+        'numpy',
+        'scipy',
+        'osgeo',
+        'matplotlib',
+        'basemap',
+        'zipfile',
+        'utide',
+        'csv',
+        'glob',
+        'gc'
+    ],
+    include_package_data=True,
+    url="https://github.com/jamal919/pyIntertidalDEM",
+    long_description=open('README.md').read(),
+)
