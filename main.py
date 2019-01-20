c024f61ed6dd3c7df2e0754972c66dfdddb3cc3c
Jamal Khan <jamal919@gmail.com>
2019-01-20 17:38:37 +0100

    refactoring

--- main.py
+++ main.py
@@ -14,13 +14,13 @@ from memory_profiler import profile
 
 # Paramters
 ## Vertref
-indatadir = '/run/media/khan/Sentinel2/Data' # Directoty of Zipped Data
+indatadir = '/run/media/khan/Sentinel2/RawData' # Directoty of Zipped Data
 outputdir = '/run/media/khan/Sentinel2/Analysis' # Directory Containing outputs
-wkdir = os.path.join(outputdir, 'Tiles') # Directory of saving unzipped data
+wkdir = os.path.join(outputdir, 'Data') # Directory of saving unzipped data
 prepdir = os.path.join(outputdir, 'PreProcessing') # Directory of saving preprocessed data   
-improcdir = os.path.join(outputdir, 'ImageProcessing') # Directory of saving processed data 
+improcdir = os.path.join(outputdir, 'ShorelineExtraction') # Directory of saving processed data 
 vertrefdir = os.path.join(outputdir, 'VerticalReferencing') # Saving Intermediate data for vertical referencing
-waterleveldir = os.path.join(vertrefdir, 'WaterLevels') # Directory of water level dat files
+waterleveldir = os.path.join(outputdir, 'WaterLevels') # Directory of water level dat files
 
 ## Preprocessing Optional Params
 stdfactor = 0.5 # Threshold for watermask , data[data>factor*std]=Land (Float) 
@@ -35,41 +35,29 @@ blob_removal_land = 10000 # Binary water map blob removal size for land features
 blob_removal_Water = 50000 # Binary water map blob removal size for water features
 
 ## Boolean Params of png saving
-prepWmaskPNGflag=False                                              # Save png for Water mask creation  (True/False)
-
-procChannelPNGflag=False                                            # Save png while channel construction (True/False)
-procWaterMapPngFlag=False                                           # Save png while binary water map creation (True/False)
-procblobRemovalpngFlag=False                                        # Save png while filtering water maps
+prepWmaskPNGflag = False                                              # Save png for Water mask creation  (True/False)
+procChannelPNGflag = False                                            # Save png while channel construction (True/False)
+procWaterMapPngFlag = False                                           # Save png while binary water map creation (True/False)
+procblobRemovalpngFlag = False                                        # Save png while filtering water maps
 
 # Selections of files to be used
-DeltaicZones = ['T45QYE','T45QWE','T45QXE','T46QCK','T46QBL','T46QBK']
-DeltaicZones = ['T46QCK']  
+DeltaicZones = ['T45QWE', 'T45QXE', 'T45QYE', 'T46QBK', 'T46QCK','T46QBL', 'T46QCL']
 
 # Worker functions
+def CreateOuputDirs(prepdir,improcdir,vertrefdir):
+    for dloc in [prepdir, improcdir, vertrefdir]:
+        if not os.path.exists(dloc):
+            os.mkdir(dloc)
 
-def CreateOupurDirs(prepdir,improcdir,vertrefdir):
-        
-    # Preprocessing directory creation
-    if not os.path.exists(prepdir):
-        os.mkdir(prepdir)
-
-    #ImageProcessing directory creation
-    if not os.path.exists(improcdir):
-        os.mkdir(improcdir)
-
-    #Vertical reference directory creation
-    if not os.path.exists(vertrefdir):
-        os.mkdir(vertrefdir)
-
-def preprocessing():
-    start_time=time.time()
+def Preprocessing():
+    start_time = time.time()
     preprep.ingest(indatadir, wkdir)
     preprep.genstat(wkdir,prepdir)
     preprep.genmask(wkdir,prepdir,dir=additionalDirectory,nstd=stdfactor,water=MaskWater,land=MaskLand,png=prepWmaskPNGflag)
-    print('Total elapsed time:',str(time.time()-start_time))
+    print('Total elapsed time:', str(time.time()-start_time))
 
-def processing():
-    DataPath=wkdir
+def ShorelineProcessing():
+    DataPath = wkdir
     Zones=os.listdir(wkdir)
     
     for zone in Zones:
@@ -78,12 +66,12 @@ def processing():
 
         for df in DataFolders:
             directory=str(os.path.join(DataPath,df,''))
-            improc.construct_channels(directory,improcdir,prepdir,png=procChannelPNGflag)
-            improc.make_watermap(directory,improcdir,prepdir,nhue=hue_channel_scaling_factor,nvalue=value_channel_scaling_factor,png=procWaterMapPngFlag)
 
             if zone in DeltaicZones:
                 improc.remove_blob(directory,improcdir,prepdir,nwater=blob_removal_Water,nland=blob_removal_land,png=procblobRemovalpngFlag)
                 improc.extract_shoreline(directory,improcdir,prepdir)
+                improc.construct_channels(directory,improcdir,prepdir,png=procChannelPNGflag)
+                improc.make_watermap(directory,improcdir,prepdir,nhue=hue_channel_scaling_factor,nvalue=value_channel_scaling_factor,png=procWaterMapPngFlag)
 
 def SingleDataProcess(directory,improcdir,prepdir):
     start_time=time.time()
@@ -98,8 +86,8 @@ def VerticalReferencing():
     vertref.set_water_levels(shorelinedir=improcdir, waterleveldir=waterleveldir, vertrefdir=vertref)
 
 if __name__=='__main__':
-
-    #CreateOupurDirs(prepdir,improcdir,vertrefdir)
-    #preprocessing()
-    #processing()
+    CreateOuputDirs(prepdir, improcdir, vertrefdir)
+    Preprocessing()
+    ShorelineProcessing()
     VerticalReferencing()
+    
