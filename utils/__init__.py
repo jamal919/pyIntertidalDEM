6d95e4035906bd6730297b2584f43e44190bb887
Jamal Khan <jamal919@gmail.com>
2019-01-21 14:00:24 +0100

    restructured module version

--- utils/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import print_function
-
-from .rivermap import RiverLineGen
-def create_rivermaps(wkdir,improcdir,preprocdir):
-    
-    RiverLineGenObj=RiverLineGen(wkdir,improcdir,preprocdir)
-    RiverLineGenObj.CombineBinaryWaterMaps()
-    
