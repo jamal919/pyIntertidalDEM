eeb415c67f2b3e289bf8eed35bcb2afb42dd353d
Jamal Khan <jamal919@gmail.com>
2019-04-26 14:48:55 +0200

    header update

--- pyintdem/dem.py
+++ pyintdem/dem.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 '''
 DEM generation using Shoreline and Water Level
 
--- pyintdem/satellite.py
+++ pyintdem/satellite.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 '''
 Complete procedure for a single satellite dataset
 
