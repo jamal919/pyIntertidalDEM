24c511a562c87285915939fdec823155c7dadeb0
Jamal Khan <jamal919@gmail.com>
2022-04-10 16:43:24 +0200

    :bug: Adds folder creation in example [fixes #6]

--- example_extract.py
+++ example_extract.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from __future__ import print_function
 from pyintdem.util import Extractor
+import os
 
 data_dir = '/run/media/khan/Sentinel2/RawData'
 out_dir='/run/media/khan/Backup KE Maxelev/Data'
+
+if not os.path.exists(out_dir):
+    os.mkdir(out_dir)
 
 extractor = Extractor(input_dir=data_dir, output_dir=out_dir)
 extractor.list_zones(debug=True)
