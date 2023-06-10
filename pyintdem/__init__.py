ad3eb4c12c0f76f72e03faee796bf6d356ae18d0
Jamal Khan <jamal919@gmail.com>
2023-06-10 14:35:24 +0200

    :package: Update packaging to pyproject [fixes #12]

---
+++ pyintdem/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""pyIntertidalDEM Sentinel-2 Processing toolbox
+
+pyIntertidalDEM is a set of libraries and procedures written in python to extract 
+shorelines from spectral images using  a ophisticated shoreline extraction algorithm. 
+These modules are developed in Python v3 environment.
+"""
+
+__version__ = '1.1'
